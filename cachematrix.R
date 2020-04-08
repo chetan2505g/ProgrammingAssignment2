d0a1b8dd21260727bc70ab9f0ceffc8e6bd19aae
chetan2505g <62828793+chetan2505g@users.noreply.github.com>
2020-04-09 02:42:07 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,16 @@
 
 ## Write a short comment describing this function
 ##Created by Chetan
-##SHA1:123456ituyvhdlsjjjey456783920kjjdgsybber
+
+# We have written 4 following method
+#set: Which set the value 
+#get: Which get the value
+#setInverse: set the inverse value of the matrix
+#getInvers: get inverse value of the matrix
+# When we pass the matrix. You can run following command and check result
+# x <- matrix(1:4,2,2)
+# m1 <- makeCacheMatrix(x)
+# For method: m1$get()
 
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
@@ -21,6 +30,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+#Below function will check if function in cache or not if it is available in cache then 
+#it will fetch from cache other wise it will run internally
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
